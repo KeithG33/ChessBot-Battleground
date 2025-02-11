86d339b79e90243fe78a183cbe1570811b3e3b7a
KeithG33 <keith.gordon9@gmail.com>
2025-02-10 21:42:37 -0800

    move config to train

---
+++ chessbot/train/config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import attridict
+import yaml
+import pprint
+
+def load_config(config_path):
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+    
+    return attridict(config)
+
+def get_config():
+    """ Return a default configuration object """
+    # load config.yaml from this file's directory
+    cfg_path = Path(__file__).parent / 'config.yaml' 
+    return load_config(cfg_path)
