fa9ae7c56e78f7d5477875d02acd0680bc04f55a
KeithG33 <keith.gordon9@gmail.com>
2025-02-10 21:40:09 -0800

    move example model to models/,m add basechessmodel to models init

--- examples/example_model/simple_chessnet.py
+++ examples/example_model/simple_chessnet.py
@@ -1,50 +1,44 @@
 import torch
 import torch.nn as nn
-from chessbot.models.base import BaseModel
+from chessbot.models.base import BaseChessModel
 
-class SimpleChessNet(BaseModel):
+
+class SimpleChessNet(BaseChessModel):
     """
-    A simple example model that extends the BaseModel for the ChessBot-Battleground library.
+    A simple example model that extends the BaseChessModel for the ChessBot-Battleground library.
     This model uses a few linear layers to process the input state and produce action logits
     and a board value.
     """
     
-    def __init__(self, device='cuda'):
+    def __init__(self):
         super().__init__()
-        self.device = device
-        piece_embed_dim = 16
         
-        self.piece_embedding = nn.Embedding(num_embeddings=13, embedding_dim=piece_embed_dim).to(device)
-
         # Simple mlp and some prediction heads
         self.layers = nn.Sequential(
             nn.Flatten(),
             nn.Linear(64, 256),  # Flatten the 8x8 board and process
             nn.ReLU(),
-            nn.Linear(256, piece_embed_dim * 64),
+            nn.Linear(256, 64),
             nn.ReLU(),
-            nn.LayerNorm(piece_embed_dim * 64)
-        ).to(self.device)
+            nn.LayerNorm(64)
+        )
 
         # Policy head
         self.policy_head = nn.Sequential(
-            nn.Linear(piece_embed_dim * 64, self.action_dim)
-        ).to(self.device)
+            nn.Linear(64, self.action_dim)
+        )
         
         # Value head
         self.value_head = nn.Sequential(
-            nn.Linear(piece_embed_dim * 64, 1),
+            nn.Linear(64, 1),
             nn.Tanh()  # Output a value between -1 and 1
-        ).to(self.device)
-    
-    def board_embedding(self, x):
-        """ Embed the board state."""
-        x = x.view(-1, 64).long()
-        return self.piece_embedding(x + 6)  # Shift the piece values to [0,13]
+        )
+
 
     def forward(self, x):
         """
-        Process the input state and produce action logits and board value.
+        Process the input state and produce action logits and board value. Note the input tensor
+        is shape (B, 8, 8) where each position is an integer representing a piece.
 
         Args:
             x (torch.Tensor): Input tensor of shape (B, 1, 8, 8)
@@ -52,9 +46,8 @@ class SimpleChessNet(BaseModel):
         Returns:
             Tuple[torch.Tensor, torch.Tensor]: 
             action_logits: shape (B, 4672)
-            board_val: shape (B,)
+            board_val: shape (B,1)
         """
-        x = self.board_embedding(x)
         features = self.layers(x)  # Reshape and pass through the network
         action_logits = self.policy_head(features)
         board_val = self.value_head(features)
