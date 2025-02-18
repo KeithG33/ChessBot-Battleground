3d8a5c1972e06be0f008b029908b6a8be25043eb
KeithG33 <keith.gordon9@gmail.com>
2025-02-17 17:09:30 -0800

    add and test inference code

---
+++ chessbot/inference/__init__.py
@@ -0,0 +1,5 @@
+from .inference import (
+    play_game,
+    play_match,
+    selfplay,
+)
--- chessbot/inference/inference.py
+++ chessbot/inference/inference.py
@@ -1,3 +1,4 @@
+from typing import Any, Optional
 import gym
 import adversarial_gym
 import chess
@@ -6,12 +7,13 @@ from chessbot.models.base import BaseChessModel
 from chessbot.mcts import MonteCarloTreeSearch
 
 
-def score_function(outcome, perspective):
-    """ Return score based on outcome (outcome of game) and perspective (color of player). """
+def score_function(outcome, perspective) -> float | int:
+    """Return score based on outcome (outcome of game) and perspective (color of player)."""
     if outcome == 0:
         return 0.5
-    
-    if (perspective == chess.WHITE and outcome == 1
+
+    if (
+        perspective == chess.WHITE and outcome == 1
         or perspective == chess.BLACK and outcome == -1
     ):
         return 1
@@ -19,31 +21,58 @@ def score_function(outcome, perspective):
     return 0
 
 
-def play_match(player1: BaseChessModel, player2: BaseChessModel, best_of=7, search=False, num_sims=250, visualize=False):
-    """ Play a match between two models and return the score of each model. """
+def play_match(
+    player1: BaseChessModel,
+    player2: BaseChessModel,
+    best_of=7,
+    search=False,
+    num_sims=250,
+    visualize=False,
+) -> tuple[int, int]:
+    """
+    Play a match between two models and return the score of each model. Scores are counted as 1 
+    for a win, 0.5 for a draw, and 0 for a loss.
+    """
     player1_score = 0
     player2_score = 0
 
+    win_condition = (best_of // 2) + 1
+
     for i in range(best_of):
         if i % 2 == 0:
-            outcome = play_game(player1, player2, search=search, num_sims=num_sims, visualize=visualize)
+            outcome = play_game(
+                player1, player2, search=search, num_sims=num_sims, visualize=visualize
+            )
             player1_score += score_function(outcome, chess.WHITE)
             player2_score += score_function(outcome, chess.BLACK)
         else:
-            outcome = play_game(player2, player1, search=search, num_sims=num_sims, visualize=visualize)
+            outcome = play_game(
+                player2, player1, search=search, num_sims=num_sims, visualize=visualize
+            )
             player1_score += score_function(outcome, chess.BLACK)
             player2_score += score_function(outcome, chess.WHITE)
+
+        if any(score >= win_condition for score in (player1_score, player2_score)):
+            break
 
     return player1_score, player2_score
 
 
-def play_game(white: BaseChessModel, black: BaseChessModel, search=False, num_sims=250, visualize=False):
+def play_game(
+    white: BaseChessModel,
+    black: BaseChessModel,
+    search=False,
+    num_sims=250,
+    visualize=False,
+) -> int:
     """
     Plays a game and returns 1 if white has won, -1 if black has won, and 0 for a draw.
     """
     step = 0
     done = False
-    env = gym.make("Chess-v0", render_mode='human') if visualize else gym.make("Chess-v0")
+    env = (
+        gym.make("Chess-v0", render_mode='human') if visualize else gym.make("Chess-v0")
+    )
     obs, info = env.reset()
 
     if search:
@@ -55,24 +84,26 @@ def play_game(white: BaseChessModel, black: BaseChessModel, search=False, num_si
         legal_moves = env.board.legal_moves
 
         if step % 2 == 0:
-            best_action, _ = white.search(
-                state, obs, simulations_number=num_sims
-            ) if search else white.get_action(obs[0], legal_moves)
+            best_action, _ = (
+                white.search(state, obs, num_simulations=num_sims)
+                if search else white.get_action(obs[0], legal_moves)
+            )
         else:
-            best_action, _ = black.search(
-                state, obs, simulations_number=num_sims
-            ) if search else black.get_action(obs[0], legal_moves)
+            best_action, _ = (
+                black.search(state, obs, num_simulations=num_sims)
+                if search else black.get_action(obs[0], legal_moves)
+            )
 
-        obs, reward, done, _, _ = env.step(best_action)
+        obs, reward, done, trunc, _ = env.step(best_action)
+
+        print(f"Reward: {reward}, Done: {done}, Step: {step}, truncated: {trunc}")
         step += 1
-    
+
     return reward
 
 
-def selfplay(model: BaseChessModel, visualize=False):
+def selfplay(model: BaseChessModel, search=True, visualize=False) -> int:
     """
     Run selfplay game with a given model.
     """
-    return play_game(model, model, visualize=visualize)
-
-     
+    return play_game(model, model, search=search, visualize=visualize)
