ea41f11423e47578b2efa5531f7f3a336d82b835
KeithG33 <keith.gordon9@gmail.com>
2025-02-18 16:34:35 -0800

    swin model cleanup for library

--- models/swin_chessnet/chess_transformer.py
+++ models/swin_chessnet/chess_transformer.py
@@ -6,6 +6,7 @@ from torch.cuda.amp import GradScaler
 from adversarial_gym.chess_env import ChessEnv
 
 
+
 class ResidualBlock(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(ResidualBlock, self).__init__()
@@ -24,15 +25,11 @@ class ResidualBlock(nn.Module):
 
 class ChessTransformer(nn.Module):
     """
-    Creates an OBM ChessNetwork that outputs a value and action for a given
+    Creates a ChessBot network that outputs a value and action for a given
     state/position. 
     
-    The network uses a feature extraction backbone from the Pytorch Image Model library (timm)
-    and feeds the ouput of that into two separate prediction heads.
-
-    The output of the policy network is a vector of size action_dim = 4672
-    and the ouput of the value network is a single value.
-
+    Uses a Swin Transformer from Timm as the feature extractor, and feeds the ouput into
+    prediction heads for action and value.
     """
 
     def __init__(self, device = 'cuda', base_lr = 0.0009, max_lr = 0.009):
@@ -68,76 +65,14 @@ class ChessTransformer(nn.Module):
             nn.Linear(num_features, 1),
             nn.Tanh()
         ).to(device)
-
-        self.critic_head = nn.Sequential(
-            ResidualBlock(num_features, 2*num_features),
-            nn.Linear(num_features, 2*num_features),
-            nn.GELU(),
-            nn.Linear(2*num_features, self.action_dim),
-            nn.Tanh()
-        ).to(device)
         
         self.value_loss = nn.MSELoss()
         self.policy_loss = nn.CrossEntropyLoss()
-        self.crit_loss = nn.MSELoss()
 
     def forward(self, x):
-        if isinstance(x, np.ndarray):
-            x = torch.tensor(x, dtype=torch.long, device=self.device)
-
         features = self.swin_transformer.forward_features(x) 
         features = features.view(features.size(0), -1)
-
         action_logits = self.policy_head(features)
-        action_rewards = self.critic_head(features)
         board_val = self.value_head(features)
 
-        return action_logits, board_val, action_rewards
-
-    def critic_loss(self, critic_output, action, result):
-        """ 
-        Generate target reward vector and compute MSE loss. Use current action and outcome of game
-        as the ground-truth.
-        """
-        batch_size = critic_output.shape[0]
-        action_inds = torch.argmax(action, dim=1)
-
-        target = torch.zeros((batch_size, 4672), dtype=torch.float32, device=self.device)
-        batch_indices = torch.arange(batch_size, device=self.device)
-        target[batch_indices, action_inds] = result.float()
-
-        return self.crit_loss(critic_output, target)
-    
-
-    def get_action(self, state, legal_moves, sample_n=1):
-        """ Randomly sample from top_n legal actions given input state"""
-
-        state = torch.as_tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0).unsqueeze(0)
-        
-        features = self.swin_transformer.forward_features(state)
-        features = features.view(features.shape[0], -1)
-        
-        policy_logits = self.policy_head(features)
-
-        legal_actions = [ChessEnv.move_to_action(move) for move in legal_moves]
-        return self.to_action(policy_logits, legal_actions, top_n=sample_n)
-    
-    def to_action(self, action_logits, legal_actions, top_n):
-        """ Randomly sample from top_n legal actions given output action logits """
-
-        if len(legal_actions) < top_n: top_n = len(legal_actions)
-
-        action_probs = torch.nn.functional.log_softmax(action_logits, dim=-1)
-        action_probs_np = action_probs.detach().cpu().numpy().flatten()
-
-        # Set non legal-actions to = -inf so they aren't considered
-        mask = np.ones(action_probs_np.shape, bool)
-        mask[legal_actions] = False
-        action_probs_np[mask] = -np.inf
-
-        # sample from indices of the top-n policy probs
-        top_n_indices = np.argpartition(action_probs_np, -top_n)[-top_n:]
-        action = np.random.choice(top_n_indices)
-        
-        log_prob = action_probs.flatten()[action]
-        return action, log_prob
+        return action_logits, board_val
