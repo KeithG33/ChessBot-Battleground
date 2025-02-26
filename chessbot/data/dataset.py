cc5c23257f220535112c2681adf3a4083222aed3
KeithG33 <keith.gordon9@gmail.com>
2025-02-26 12:31:46 -0800

    fix condition for parallel num_threads in dataset loading. should be <=.
    Otherwise minor formating and cleaning and docstrings/commenting

--- chessbot/data/dataset.py
+++ chessbot/data/dataset.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 import chess
 import chess.pgn
 
@@ -18,45 +19,52 @@ def create_sparse_vector(action_probs):
     return sparse_vector
 
 
+def result_to_number(result):
+    if result == "1-0":
+        return 1
+    if result == "0-1":
+        return -1
+    return 0
+
+
 class ChessDataset(Dataset):
-    def __init__(self, pgn_files, num_threads=0):
+    """
+    The PyTorch chess dataset to load and process game data from PGN files.
+
+    For each board position, the dataset extracts:
+      - The board state.
+      - The move taken (action), represented as a one-hot vector of shape (4672,)
+      - The game result: 1 for win, -1 for loss, 0 for draw
+
+    The dataset supports both sequential and parallel processing of PGN files
+    with the `num_threads` argument.
+    """
+
+    def __init__(self, pgn_files, num_threads = 0):
         super().__init__()
         self.load_pgn_files(pgn_files)
-
         self.data = (
-            self.generate_pgn_data()
-            if num_threads < 1
-            else self.generate_pgn_data_parallel(num_threads)
+            self.generate_data()
+            if num_threads <= 1
+            else self.generate_data_parallel(num_threads)
         )
 
     def load_pgn_files(self, pgn_files):
-        # Handle file or directory
+        """Handle loading pgn files from a directory, file, or list of files"""
         if isinstance(pgn_files, str):
             if Path(pgn_files).is_dir():
                 self.pgn_files = list(Path(pgn_files).rglob("*.pgn"))
             elif Path(pgn_files).is_file():
                 self.pgn_files = [pgn_files]
             else:
-                raise ValueError("Invalid pgn_files argument - string not a file or directory")
-        # Handle list of files
+                raise ValueError("Invalid pgn_files argument - string not a file or directory"
+                )
         elif isinstance(pgn_files, list):
             self.pgn_files = pgn_files
-        # Handle no bueno
         else:
             raise ValueError("Invalid pgn_files argument")
 
-    def generate_pgn_data(self):
-        """Return a list of tuples with (state, action, result)"""
-        data = []
-        for pgn_file in self.pgn_files:
-            with open(pgn_file) as pgn:
-                while (game := chess.pgn.read_game(pgn)) is not None:
-                    state_maps, actions, results = self.get_game_data(game)
-                    game_data = list(zip(state_maps, actions, results))
-                    data.extend(game_data)
-        return data
-
-    def generate_pgn_data_single(self, pgn_file):
+    def generate_pgn_data(self, pgn_file):
         data = []
         with open(pgn_file) as pgn:
             while (game := chess.pgn.read_game(pgn)) is not None:
@@ -64,15 +72,22 @@ class ChessDataset(Dataset):
                 game_data = list(zip(state_maps, actions, results))
                 data.extend(game_data)
         return data
+    
+    def generate_data(self):
+        """Return a list of tuples of (state, action, result)"""
+        data = []
+        for pgn_file in self.pgn_files:
+            data.extend(self.generate_pgn_data(pgn_file))
+        return data
 
-    def generate_pgn_data_parallel(self, num_threads):
+    def generate_data_parallel(self, num_threads):
         try:
             set_start_method('spawn', force=True)
         except RuntimeError as err:
             print(f"Warning: {err}")
 
         with Pool(num_threads) as pool:
-            results = pool.map(self.generate_pgn_data_single, self.pgn_files)
+            results = pool.map(self.generate_pgn_data, self.pgn_files)
 
         return [item for sublist in results for item in sublist]
 
@@ -87,7 +102,7 @@ class ChessDataset(Dataset):
 
         board = game.board()
         result = game.headers['Result']
-        result = self.result_to_number(result)
+        result = result_to_number(result)
         for move in game.mainline_moves():
             canon_state = self.get_canonical_state(board, board.turn)
             action = ChessEnv.move_to_action(move)
@@ -108,14 +123,6 @@ class ChessDataset(Dataset):
         state = ChessEnv.get_piece_configuration(board)
         state = state if turn else -state
         return state
-
-    def result_to_number(self, result):
-        if result == "1-0":
-            return 1
-        if result == "0-1":
-            return -1
-        else:
-            return 0
 
     def __len__(self):
         return len(self.data)
