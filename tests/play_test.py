1ede8de74b44a878185df1c79b296c4c1caf57e2
KeithG33 <keith.gordon9@gmail.com>
2025-08-15 20:19:11 -0700

    use load_with_weights as exmapl in play test

--- tests/play_test.py
+++ tests/play_test.py
@@ -2,9 +2,9 @@ from chessbot.models import MODEL_REGISTRY
 from chessbot.inference import selfplay, run_match
 
 # Run selfplay
-model1 = MODEL_REGISTRY.load_model("simple_chessbot")
-outcome = selfplay(model1, search=True, visualize=False, sample=True) # 1 if white wins, -1 if black wins, 0 draw
+model1 = MODEL_REGISTRY.load_with_weights("swin_chessbot", "KeithG33/swin_chessbot")
+outcome = selfplay(model1, search=True, visualize=True, sample=True) # 1 if white wins, -1 if black wins, 0 draw
 
 # Play a match with two models, use MCTS
-model2 = MODEL_REGISTRY.load_model("simple_chessbot")
+model2 = MODEL_REGISTRY.load_with_weights("swin_chessbot", "KeithG33/swin_chessbot")
 scores = run_match(model1, model2, best_of=3, search=True, visualize=True) # Returns (score1, score2)
